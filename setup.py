035ae5b579f6864331d71311214ed23d557c10b4
David Bain <david@alteroo.com>
2014-10-18 22:10:15 -0500

    updated setup.py to add gloss.theme as a dependency

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(name='gloss.policy',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'gloss.theme',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
@@ -33,6 +34,4 @@ setup(name='gloss.policy',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
